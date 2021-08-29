17b2330443728017c41cec4e4acdbd42c5474807
thenatural86 <philip.smalls86@gmail.com>
2021-08-29 13:18:50 -0400

    Removes sorting logic

--- index.js
+++ index.js
@@ -12,37 +12,3 @@ const printString = (myString) => {
 }
 
 printString('pizza')
-
-let sortedArray = []
-let unsortedArray = [5, 6, -1, 1, 3]
-
-minAndRemove(sortedArray, unsortedArray)
-
-const minAndRemove = (array) => {
-  let min = array[0]
-  let minIndex = 0
-
-  for (let i = 0; i < array.length - 1; i++) {
-    if (array[i] < min) {
-      min = array[i]
-      minIndex = i
-    }
-  }
-  array.splice(minIndex, 1)
-  return min
-}
-
-const selectionSort = (array) => {
-  let newMin
-  let sorted = []
-
-  while (array.length != 0) {
-    newMin = minAndRemove(array)
-    // sorted.push(newMin)
-    sorted = [...sorted, newMin]
-  }
-  return sorted
-}
-
-// Selection sort process:
-// find the minimum and remove elements until there are no more elements left in the original array
